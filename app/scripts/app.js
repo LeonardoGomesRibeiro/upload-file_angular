7ef10b9c6e38ff249138516c29d2ab38cfbc629c
Leonardo Ribeiro <leonardo.ribeiro@ifactory.com.br>
2016-11-08 14:55:23 -0300

    added upload directive.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,4 +2,32 @@
 angular.module('dotSubApp', [])
 .controller('UploadController', ['$scope', function($scope) {
    
-}]);
+}])
+.directive("fileread", function () {
+    return {
+      scope: {
+        fileread: "="
+      },
+      link: function (scope, element, attributes) {
+        element.bind("change", function (changeEvent) {
+          var readers = [] ,
+              files = changeEvent.target.files ,
+              datas = [] ;
+          for ( var i = 0 ; i < files.length ; i++ ) {
+            readers[ i ] = new FileReader();
+            readers[ i ].onload = function (loadEvent) {
+              datas.push( loadEvent.target.result );
+              if ( datas.length === files.length ){
+                scope.$apply(function () {
+                  scope.fileread = datas;
+                });
+              }
+            }
+            readers[ i ].readAsDataURL( files[i] );
+          }
+        });
+
+      }
+    }
+  });
+;
