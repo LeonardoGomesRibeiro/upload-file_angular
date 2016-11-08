0a243d05f59465e7f9eff649acc84b5b6983b32f
Leonardo Ribeiro <leonardo.ribeiro@ifactory.com.br>
2016-11-08 15:13:52 -0300

    added spring dispatcher servlet and configured controller to handle the upload.

--- backend/uploadFile/src/main/java/com/dotsub/codetest/controllers/UploadFileController.java
+++ backend/uploadFile/src/main/java/com/dotsub/codetest/controllers/UploadFileController.java
@@ -1,14 +1,40 @@
 package com.dotsub.codetest.controllers;
 
+import java.io.File;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UploadFileController {
 
 	@RequestMapping("/rest/uploadFile")
-	public ModelAndView upload() {
-		return null;
+	public @ResponseBody String upload(HttpServletRequest request) {
+		try {
+	        List<FileItem> multipart = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+	        String folder = "path";
+	        String filename = "";
+	        for (FileItem item : multipart) {
+	            if (!item.isFormField()) {
+	                filename = new File(item.getName()).getName();
+	                File file = new File(folder);
+	                if (!file.exists()) {
+	                    file.mkdir();
+	                }
+	            }
+				item.write(new File(folder + "/" + filename));
+	        }
+	    } catch(Exception e) {
+	    	e.printStackTrace();
+	    }
+		
+		return "ok";
 	}
 }
