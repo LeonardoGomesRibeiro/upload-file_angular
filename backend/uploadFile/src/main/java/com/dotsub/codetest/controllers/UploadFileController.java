cdfbac166e7c476b31c64d8d3a0f6dfcdf9f841d
Leonardo Gomes Ribeiro <leonardometalhead@gmail.com>
2016-11-09 20:25:07 -0300

    merge local files

--- backend/uploadFile/src/main/java/com/dotsub/codetest/controllers/UploadFileController.java
+++ backend/uploadFile/src/main/java/com/dotsub/codetest/controllers/UploadFileController.java
@@ -1,58 +1,80 @@
 package com.dotsub.codetest.controllers;
 
 import java.io.File;
-import java.util.Date;
+import java.util.Enumeration;
+import java.util.LinkedList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UploadFileController {
 	
-	@Autowired
+	List<com.dotsub.codetest.model.File> uploadedFiles = new LinkedList<com.dotsub.codetest.model.File>();
+	
+	@RequestMapping("/list")
+	public ModelAndView listAll() {
+		ModelAndView mav = new ModelAndView("index");
+		
+		return mav;
+	}
+	
+	@RequestMapping("/upload")
+	public ModelAndView upload() {
+		ModelAndView mav = new ModelAndView("index");
+		
+		return mav;
+	}
+/*	@Autowired
 	SessionFactory sessionFactory;
 	
-    private Session currentSession;
+    private Session currentSession;*/
     
-    @PostConstruct
+   /* @PostConstruct
     public void init(){
     	currentSession = sessionFactory.getCurrentSession();
-    }
+    }*/
 	@RequestMapping("/rest/uploadFile")
 	public @ResponseBody String upload(HttpServletRequest request) {
 		try {
 	        List<FileItem> multipart = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
 	        String folder = "path";
 	        String filename = "";
+	        
 	        for (FileItem item : multipart) {
-	            if (!item.isFormField()) {
+	        	com.dotsub.codetest.model.File file = new com.dotsub.codetest.model.File();					        	
+				
+	        	file.setImagePath(folder + "/" + filename);
+	        	if (!item.isFormField()) {
 	                filename = new File(item.getName()).getName();
-	                File file = new File(folder);
+	                file.setTitle(filename);
+	                /*File file = new File(folder);
 	                if (!file.exists()) {
 	                    file.mkdir();
 	                }
+	                */
+	            } else {
+	            	if (item.getFieldName().equals("description")){
+		        		file.setDescription(item.getString());	        		
+		        	} else if (item.getFieldName().equals("createdDate")) {
+		        		//SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy", Locale.US);	        	
+			        	//Date d = sdf.parse(item.getString());
+			        	//file.setCreatedDate(d);
+		        	}
 	            }
-				item.write(new File(folder + "/" + filename));
-				
-				com.dotsub.codetest.model.File file = new com.dotsub.codetest.model.File();
-				file.setCreatedDate(new Date());
-				file.setTitle(filename);
-				file.setDescription("my description");
-				file.setImagePath(folder + "/" + filename);
-				
-				currentSession.persist(file);
-		        currentSession.flush();
+	        	uploadedFiles.add(file);
+				//item.write(new File(folder + "/" + filename));
+	        	
+				/*currentSession.persist(file);
+		        currentSession.flush();*/
 	        }
 	    } catch(Exception e) {
 	    	e.printStackTrace();
--- backend/uploadFile/src/main/webapp/resources/scripts/app.js
+++ backend/uploadFile/src/main/webapp/resources/scripts/app.js
@@ -1,11 +1,15 @@
 'use strict';
-angular.module('dotSubApp', [])
-.controller('UploadController', ['$scope','fileUpload', function($scope, fileUpload) {
-	$scope.uploadFile = function(){
-        var file = $scope.myFile;
-        console.log('file is ' );
-        console.dir(file);
-        var uploadUrl = "/uploadFile/rest/uploadFile";
-        fileUpload.uploadFileToUrl(file, uploadUrl);
-    };
-}]);
+angular.module('dotSubApp', ['ngRoute'])
+.config(function($routeProvider) {
+    $routeProvider
+    .when("/upload", {
+        templateUrl : "templates/upload.jsp",
+        controller: 'UploadFileController'
+    })
+    .when("/list", {
+        templateUrl : "templates/listAll.jsp",
+        controller: 'ListFilesController'
+    }).otherwise({
+		redirectTo: '/upload'
+    });
+});
